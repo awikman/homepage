a9004f39db794715833af9693fa3a08f5d24ca30
Aleksi Wikman <aleksi.wikman@gmail.com>
2019-11-03 17:38:12 +0100

    Add a proper footer.

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 export function Footer(props){
     return (
         <footer className="Footer">
-          <div>TODO: Footer here...</div>
+          <div>
+            <div>Contact me at: <a className="Link" href="mailto:aleksi@wikman.ch">aleksi@wikman.ch</a></div>
+            <div>Acces the source code at: <a className="Link" href="https://github.com/awikman/homepage">Github</a></div>
+          </div>
         </footer>
 
     );
