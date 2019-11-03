2859c714ccf769b6eb3bd745e4a1d20625605d0e
Aleksi Wikman <aleksi.wikman@gmail.com>
2019-11-03 20:44:16 +0100

    Fix the profile picture styles
    Make it square, so that we get a proper circle of it.
    
    Add the height and width parameters to the img tag. The final size might be different on some clients, but it doesn't matter much. The css will override it if needed.
    
    Make it just a bit larger. Small enough change that it wont make the header any larger.

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,9 @@ export function Header(props){
     return (
         <header className="Header">
           <div className="HeaderContent">
-            <img src={process.env.PUBLIC_URL + "profile_image.jpg"} className="ProfileImage" alt="profile image" />
+            <img src={process.env.PUBLIC_URL + "profile_image.jpg"}
+                 width="96" height="96"
+                 className="ProfileImage" alt="profile image" />
             <div className="Profile">
               <h1>Aleksi Wikman</h1>
               <h2>Software Engineer</h2>
