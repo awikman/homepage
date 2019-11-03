8094b61a0b565989356c852de3861e5e0d468302
Aleksi Wikman <aleksi.wikman@gmail.com>
2019-11-03 17:51:06 +0100

    Fix the link in the footer, and add a external link icon

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,12 @@ export function Footer(props){
     return (
         <footer className="Footer">
           <div>
-            <div>Contact me at: <a className="Link" href="mailto:aleksi@wikman.ch">aleksi@wikman.ch</a></div>
-            <div>Acces the source code at: <a className="Link" href="https://github.com/awikman/homepage">Github</a></div>
+            <div>Contact me at <a className="Link" href="mailto:aleksi@wikman.ch">aleksi@wikman.ch</a></div>
+            <div>Acces the source code at
+              <a className="Link" rel="noopener" target="_blank" href="https://github.com/awikman/homepage">
+                <i className="fas fa-external-link-alt"></i> Github
+              </a>
+            </div>
           </div>
         </footer>
 
